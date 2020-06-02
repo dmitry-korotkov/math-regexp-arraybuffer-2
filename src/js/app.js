c488fbee7fb47951b95e56061682316e744fe9dd
Dmitry Korotkov <korotkov.dmitriy10.1@gmail.com>
2020-06-02 23:10:48 +0400

    fix code

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,28 @@
+/* eslint-disable no-useless-escape */
 export default class Validator {
-    validateUsername(userName) {
-      if (userName.match(/^[a-z]/i)) {
-        this.userName = userName;
-      } else {
-        throw new Error('Имя пользователя должно начинаться с буквы латинского алфавита');
-      }
-  
-      if (userName.match(/[^a-z]$/)) {
-        throw new Error('Имя должно заканчиваться на букву латинского алфавита');
-      } else {
-        this.userName = userName;
-      }
-  
-      if (!userName.match(/\d{4}/)) {
-        this.userName = userName;
-      } else {
-        throw new Error('Имя пользователя не должно содержать подряд более трёх цифр.');
-      }
-  
-      if (!userName.match(/[^a-z\-\_\d]/)) {
-        this.userName = userName;
-      } else {
-        throw new Error('Имя пользователя не должно содержать симлволов за исключением: тире -, подчёркивания _.');
-      }
+  validateUsername(userName) {
+    if (userName.match(/^[a-z]/i)) {
+      this.userName = userName;
+    } else {
+      throw new Error('Имя пользователя должно начинаться с буквы латинского алфавита');
+    }
+
+    if (userName.match(/[^a-z]$/)) {
+      throw new Error('Имя должно заканчиваться на букву латинского алфавита');
+    } else {
+      this.userName = userName;
+    }
+
+    if (!userName.match(/\d{4}/)) {
+      this.userName = userName;
+    } else {
+      throw new Error('Имя пользователя не должно содержать подряд более трёх цифр.');
+    }
+
+    if (!userName.match(/[^a-z\-\_\d]/)) {
+      this.userName = userName;
+    } else {
+      throw new Error('Имя пользователя не должно содержать симлволов за исключением: тире -, подчёркивания _.');
     }
   }
+}
